3deda72986ba4260346d3e4c8857c1d0a58de262
Ondřej Častoral <124709424+castixz@users.noreply.github.com>
2026-05-21 14:17:14 +0200

    init commit

---
+++ src/main/java/com/github/castixz/marketshare/MarketShareCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.castixz.marketshare;
+
+import java.util.logging.Logger;
+
+public class MarketShareCalculatorApplication {
+
+    private static final Logger LOG = Logger.getLogger(MarketShareCalculatorApplication.class.getName());
+
+    static void main(String[] args) {
+        LOG.info("hello world");
+    }
+
+}
